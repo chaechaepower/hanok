4795b1ea6ee9d03672200bed819a2f0a33a80e52
배재유 <akakehcn@gmail.com>
2026-03-09 10:53:30 +0900

    [S14P21D105-87] fix : update socket config

--- be/src/main/java/com/ssafy/be/global/websocket/config/WebSocketConfig.java
+++ be/src/main/java/com/ssafy/be/global/websocket/config/WebSocketConfig.java
@@ -16,6 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/broadcast", "/private");
         registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
     }
 
     @Override
