619c2ad84444738e1417cb12c2592a39ef9be41d
배재유 <akakehcn@gmail.com>
2026-03-09 10:31:08 +0900

    [S14P21D105-87] feat : add websocket config + common response

---
+++ be/src/main/java/com/ssafy/be/global/websocket/dto/StompResponse.java
@@ -0,0 +1,21 @@
+package com.ssafy.be.global.websocket.dto;
+
+import com.ssafy.be.global.websocket.enums.StompType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StompResponse<T> {
+
+    private StompType eventType;
+    private T payload;
+    private LocalDateTime timestamp;
+
+}
