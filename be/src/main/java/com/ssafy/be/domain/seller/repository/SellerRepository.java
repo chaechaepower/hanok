2364f16de57a153824db6fb12dd3008d4dca87ae
chaechaepower <cy6kimm@gmail.com>
2026-03-11 03:12:06 +0900

    [S14P21D105-59] fix: delete duplicate method

--- be/src/main/java/com/ssafy/be/domain/seller/repository/SellerRepository.java
+++ be/src/main/java/com/ssafy/be/domain/seller/repository/SellerRepository.java
@@ -4,13 +4,8 @@ import com.ssafy.be.domain.seller.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-import java.util.Optional;
-
 public interface SellerRepository extends JpaRepository<Seller, Long> {
     Optional<Seller> findByUserId(Long userId);
 
     boolean existsByUserId(Long userId);
-
-    Optional<Seller> findByUserId(Long userId);
-
 }
