409ddfd4fe753fd1f4ebf5384b3bf8c5984db3da
김강연 <dio0517@naver.com>
2026-03-10 16:50:18 +0900

    fix: remove duplicate import and fix UserErrorCode

--- be/src/main/java/com/ssafy/be/domain/seller/repository/SellerRepository.java
+++ be/src/main/java/com/ssafy/be/domain/seller/repository/SellerRepository.java
@@ -4,10 +4,7 @@ import com.ssafy.be.domain.seller.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-import java.util.Optional;
-
 public interface SellerRepository extends JpaRepository<Seller, Long> {
-    Optional<Seller> findByUserId(Long userId);
 
     boolean existsByUserId(Long userId);
 
