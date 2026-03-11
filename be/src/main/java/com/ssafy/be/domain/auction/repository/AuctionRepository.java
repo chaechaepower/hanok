a9e81203518e3079ade0f98d2452f2c2080e953a
김강연 <dio0517@naver.com>
2026-03-11 20:09:41 +0900

    [S14P21D105-56] fix : add item id

--- be/src/main/java/com/ssafy/be/domain/auction/repository/AuctionRepository.java
+++ be/src/main/java/com/ssafy/be/domain/auction/repository/AuctionRepository.java
@@ -3,7 +3,9 @@ package com.ssafy.be.domain.auction.repository;
 import com.ssafy.be.domain.auction.entity.Auction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
+    List<Auction> findByStreamId(Long streamId);
 }
