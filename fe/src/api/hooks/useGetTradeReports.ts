b386914a131e3ac4efc1dce13e1350b6452716f7
이효은 <lhe4528@naver.com>
2026-03-17 09:08:07 +0900

    hotfix: change trade reports api path

--- fe/src/api/hooks/useGetTradeReports.ts
+++ fe/src/api/hooks/useGetTradeReports.ts
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getFetchInstance } from '../instance';
 import type { ApiResponse, TradeReportItem, TradeReportType } from '@/types';
 
-export const getTradeReportsPath = () => '/v1/trade/reports';
+export const getTradeReportsPath = () => '/v1/trade-reports';
 
 export const getTradeReports = async (type: TradeReportType) => {
   const response = await getFetchInstance().get<ApiResponse<TradeReportItem[]>>(getTradeReportsPath(), {
