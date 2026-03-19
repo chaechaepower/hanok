8663df97e3653a9e0d1acb20e7ebe348b281a87d
김강연 <dio0517@naver.com>
2026-03-19 10:42:32 +0900

    fix: validate stream status on delete and clear viewers on stream end

--- be/src/main/java/com/ssafy/be/domain/stream/exception/StreamErrorCode.java
+++ be/src/main/java/com/ssafy/be/domain/stream/exception/StreamErrorCode.java
@@ -12,6 +12,7 @@ public enum StreamErrorCode implements ErrorCode {
     THUMBNAIL_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "STREAM_002", "썸네일 업로드에 실패했습니다."),
     INVALID_STREAM_EVENT_TYPE(HttpStatus.NOT_FOUND, "STREAM_003", "존재하지 않는 스트림 이벤트 타입입니다."),
     STREAM_NOT_LIVE(HttpStatus.BAD_REQUEST, "STREAM_004", "라이브 상태의 스트림이 아닙니다."),
+    STREAM_CANNOT_DELETE(HttpStatus.BAD_REQUEST, "STREAM_005", "예약 상태의 방송만 삭제할 수 있습니다."),
     ;
 
     private final HttpStatus httpStatus;
