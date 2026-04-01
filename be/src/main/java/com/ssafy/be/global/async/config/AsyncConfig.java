ce594931b5dd74136d0197a304d6e14e52d4a78c
Chaeyoon Kim <cy6kimm@gmail.com>
2026-04-02 04:28:27 +0900

    chore: add thumbnail executor metric

--- be/src/main/java/com/ssafy/be/global/async/config/AsyncConfig.java
+++ be/src/main/java/com/ssafy/be/global/async/config/AsyncConfig.java
@@ -1,23 +1,34 @@
 package com.ssafy.be.global.async.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.Collections;
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
     @Bean("thumbnailExecutor")
-    public Executor thumbnailExecutor() {
+    public Executor thumbnailExecutor(MeterRegistry meterRegistry) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(5);
         executor.setQueueCapacity(20);
         executor.setThreadNamePrefix("thumbnail-");
         executor.initialize();
+
+        ExecutorServiceMetrics.monitor(
+                meterRegistry,
+                executor.getThreadPoolExecutor(),
+                "thumbnail_executor",
+                Collections.emptyList()
+        );
+
         return executor;
     }
 }
